1e4e0a1374b5a93df3ed2f033c765219a5fa3459
MyMac <mymac@Joels-MacBook-Pro.local>
2021-04-19 13:52:51 -0500

    first commit

---
+++ src/main/java/com/codingdojo/mvc/services/BookService.java
@@ -0,0 +1,50 @@
+package com.codingdojo.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+	// adding the book repository as a dependency
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+	// returns all the books
+	public List<Book> allBooks(){
+		return bookRepository.findAll();
+	}
+	
+	// create a book
+	public Book createBook(Book b) {
+		return bookRepository.save(b);
+	}
+	
+	// retrieves a book
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if(optionalBook.isPresent()) {
+			return optionalBook.get();
+		}
+		else {
+			return null;
+		}
+	}
+	
+	public void deleteBook(Long id) {
+		Book book = findBook(id);
+		bookRepository.delete(book);
+	}
+	
+	public Book updateBook(Book b) {
+		return bookRepository.save(b);
+	}
+
+}
